9b5e054c19d0ae108ff586882d13aa6af106854a
jbaxx <jbf8dof@outlook.com>
2019-12-05 21:48:28 +0000

    add nary tree package with its block printing methods

---
+++ nary/nary.go
@@ -0,0 +1,127 @@
+// Package narytree defines a simple n-ary Tree data structure
+package narytree
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// NaryTree defines an n-ary Tree data structure
+type NaryTree struct {
+	Root *Node
+}
+
+// Node defines a node element of an NaryTree
+type Node struct {
+	Key   int
+	Data  string
+	Nodes []*Node
+}
+
+// String implements the stringer interface for the Node
+// printing the Key and Data together
+func (n *Node) String() string {
+	return "[" + strconv.Itoa(n.Key) + "] " + n.Data
+}
+
+// PrintBlock prints the NaryTree in a format similar to the unix command: tree
+func (h *NaryTree) PrintBlock() {
+	lost := []bool{}
+	h.Root.printBlock(lost)
+}
+
+func (t *Node) printBlock(sl stackLast) {
+
+	fmt.Printf("%s%s\n", sl, t)
+
+	// get len of node Nodes array, minus 1 to make len 0-based
+	slong := len(t.Nodes) - 1
+	for i, v := range t.Nodes {
+		// compare len to index to know if this is the last elem
+		isLast := i == slong
+		// append (push) to the "is last" stack
+		sl = append(sl, isLast)
+
+		v.printBlock(sl)
+		// when the recursive call returns, pop the last elemt
+		sl = sl[:len(sl)-1]
+	}
+
+}
+
+// stackLast is a stack to track for each ancestor
+// if it's the last member of it's own slice, this way we know
+// if it has to be represented with an elbow character as well as
+// which one to not print when nested in a tree.
+type stackLast []bool
+
+// String implements the stringer interface, to pass the stackLast to the io Writer
+// This is a section of a printed tree along with the stackLast trace
+// for each node and its ancestors.
+// If the node itself is last in its own []*Node slice we preceed it with an elbow: └───
+// If the node isn't the last in its own []*Node slice we preceed it with a continue elbow: ├───
+// For the stack trace, if an ancestor is not the last, we print its continuation trace: │
+// Otherwise we print a blank space, below represented with: *
+//
+// ├─── 19                           [19]false
+// │    ├─── 25                      [19]false [25]false
+// │    │    └─── 26                 [19]false [25]false [26]true
+// │    │    *    └─── 36            [19]false [25]false [26]true [36]true
+// │    │    *    *    ├─── 37       [19]false [25]false [26]true [36]true [37]false
+// │    │    *    *    ├─── 38       [19]false [25]false [26]true [36]true [38]false
+// │    │    *    *    ├─── 39       [19]false [25]false [26]true [36]true [39]false
+// │    │    *    *    │    ├─── 52  [19]false [25]false [26]true [36]true [39]false [52]false
+func (s stackLast) String() string {
+	if len(s) == 0 {
+		return ""
+	}
+	ng := []string{}
+	last, rest := s[len(s)-1], s[:len(s)-1]
+	for _, r := range rest {
+		if r {
+			ng = append(ng, "     ")
+		} else {
+			ng = append(ng, "│    ")
+		}
+	}
+	if last {
+		ng = append(ng, "└─── ")
+	} else {
+		ng = append(ng, "├─── ")
+	}
+	ngj := strings.Join(ng, "")
+	return ngj
+}
+
+// PrintTree prints a pre order traversal of the NaryTree
+func (h *NaryTree) PrintTree(fn func(int)) {
+	h.Root.printTurn(fn)
+}
+
+func (t *Node) printTurn(fn func(int)) {
+
+	fn(t.Key)
+
+	for _, v := range t.Nodes {
+		v.printTurn(fn)
+	}
+}
+
+// PrintPaths prints all the paths from root to each leaf node in the NaryTree
+func (h *NaryTree) PrintPaths() {
+	var q []int
+	h.Root.printPaths(q)
+}
+
+func (t *Node) printPaths(q []int) {
+	q = append(q, t.Key)
+	if len(t.Nodes) == 0 {
+		fmt.Println(q)
+	}
+
+	for _, v := range t.Nodes {
+		v.printPaths(q)
+	}
+	q = q[:len(q)-1]
+}
---
+++ nary/nary_test.go
@@ -0,0 +1,76 @@
+package narytree
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestPrintTree(t *testing.T) {
+
+	h := &NaryTree{
+		Root: &Node{
+			Key:  1,
+			Data: "uno",
+			Nodes: []*Node{
+				&Node{Key: 3, Data: "tres", Nodes: []*Node{
+					&Node{Key: 5, Data: "cinco"},
+					&Node{Key: 8, Data: "ocho"}},
+				},
+				&Node{Key: 6, Data: "seis", Nodes: []*Node{
+					&Node{Key: 17, Data: "diecisiete"},
+					&Node{Key: 18, Data: "dieciocho"},
+					&Node{Key: 19, Data: "diecinueve", Nodes: []*Node{
+						&Node{Key: 25, Data: "veinticinco", Nodes: []*Node{&Node{Key: 26, Data: "veintiseis", Nodes: []*Node{
+							&Node{Key: 306, Data: "trescientos seis", Nodes: []*Node{
+								&Node{Key: 317, Data: "trescientos diecisiete"},
+								&Node{Key: 318, Data: "trescientos dieciocho"},
+								&Node{Key: 319, Data: "trescientos diecinueve", Nodes: []*Node{
+									&Node{Key: 325, Data: "trescientos veinticinco"},
+									&Node{Key: 329, Data: "trescientos veintinueve"},
+								}},
+								&Node{Key: 339, Data: "trescientos treinta y nueve"},
+								&Node{Key: 349, Data: "trescientos cuarenta y nueve"},
+							},
+							}}}}},
+						&Node{Key: 29, Data: "veintinueve"},
+					}},
+					&Node{Key: 39, Data: "treinta y nueve"},
+					&Node{Key: 49, Data: "cuarenta y nueve"},
+				},
+				},
+				&Node{Key: 73, Data: "setenta y tres", Nodes: []*Node{
+					&Node{Key: 85, Data: "ochenta y cinco"},
+					&Node{Key: 88, Data: "ochenta y ocho", Nodes: []*Node{
+						&Node{Key: 91, Data: "noventa y uno", Nodes: []*Node{&Node{Key: 94, Data: "noventa y cuatro"}}},
+					}}},
+				},
+				&Node{Key: 7, Data: "siete"},
+			},
+		},
+	}
+
+	fmt.Println("PrintTree: ")
+	h.PrintTree(func(v int) { fmt.Println(v) })
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("PrintPaths: ")
+	h.PrintPaths()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("PrintBlock: ")
+	h.PrintBlock()
+	fmt.Println()
+
+	want := []int{1, 3, 5, 8, 6, 17, 18, 19, 25, 26, 306, 317, 318, 319, 325, 329, 339, 349, 29, 39, 49, 73, 85, 88, 91, 94, 7}
+	// want := []int{1, 3, 5, 8, 6, 17, 18, 19, 39, 49, 7}
+	got := []int{}
+	h.PrintTree(func(v int) {
+		got = append(got, v)
+	})
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v; want %v", got, want)
+	}
+
+}
